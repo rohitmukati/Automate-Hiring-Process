ba1fb74ff403599112fd726dd7b9e115fcc8d911
mukatirohit <rohanmukati2002@gmail.com>
2025-03-22 12:29:05 +0530

    update_sheet updated

--- Agent2/update_sheet.py
+++ Agent2/update_sheet.py
@@ -1,12 +1,17 @@
 import gspread
 from google.oauth2.service_account import Credentials
 import json
+from dotenv import load_dotenv
+import os
 
 # 🔹 Service Account JSON file ka path (Tumhare downloaded JSON file ka actual path yahan daalo)
 
+load_dotenv()
+GOOGLE_SHEET_ID = os.getenv("GOOGLE_SHEET_ID")
+GOOGLE_SHEET_JSON_PATH = os.getenv("GOOGLE_SHEET_JSON_PATH")
 
 
-def update_sheet(path, json_data, score):
+def update_sheet(json_data, score):
     print("Received JSON Data FOR Sheet Updating:", repr(json_data))
     json_data = json_data.strip()  # Extra spaces & newlines remove karo
 
@@ -36,7 +41,7 @@ def update_sheet(path, json_data, score):
     # phone = data.get("Phone")
     # city = data.get("City")
     
-    SERVICE_ACCOUNT_FILE = path
+    SERVICE_ACCOUNT_FILE = GOOGLE_SHEET_JSON_PATH
 
 # 🔹 Scope define karo (Google Sheets access ke liye)
     SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
@@ -48,7 +53,7 @@ def update_sheet(path, json_data, score):
     client = gspread.authorize(creds)
 
 # 🔹 Apne Google Sheet ka ID daalo (Sheet URL me se milega)
-    SHEET_ID = "1UKwOWCieC1-933nJMooSznGV9EPGZ16ncHQwvViqBqw"
+    SHEET_ID = GOOGLE_SHEET_ID
 
 # 🔹 Sheet open karo
     sheet = client.open_by_key(SHEET_ID).sheet1  # First sheet select
--- main.py
+++ main.py
@@ -6,11 +6,10 @@ from Agent2.matching import get_score
 from Agent2.update_sheet import update_sheet
 
 if __name__ == "__main__":
-    pdf_path = "ATS classic HR resume.docx"
+    pdf_path = "Genai.pdf"
     jd = """We are looking for a skilled Java Developer to design, develop, and maintain high-performance applications. The ideal candidate should have strong experience with Java, Spring Boot, Hibernate, and RESTful APIs. You will be responsible for writing clean, scalable code and ensuring application performance and security. Experience with microservices architecture, Docker, and Kubernetes is highly desirable. The candidate should be proficient in working with SQL and NoSQL databases like MySQL, PostgreSQL, and MongoDB. Familiarity with JUnit and other testing frameworks, along with experience in CI/CD pipelines using Jenkins or GitLab, is a plus. Strong understanding of OOP principles, design patterns, and multithreading is required. The candidate will work closely with cross-functional teams to define and implement new features, resolve bugs, and ensure smooth production deployment. Experience with AWS, GCP, or Azure for cloud-based development and deployment will be an added advantage."""
     data  = run_agent1(pdf_path)
     print(data)
     score = get_score(data, jd)
     print(score)
-    path = "gen-lang-client-0812795063-300e2cb76181.json"
-    update_sheet(path, data, score)
+    update_sheet(data, score)
